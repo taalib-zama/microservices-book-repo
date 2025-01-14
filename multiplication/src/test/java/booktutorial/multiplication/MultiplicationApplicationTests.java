d3e8fb74558cdce23b1ada5445daf6b1c1294c3d
taalib-zama <taalib.zama@gmail.com>
2025-01-14 15:25:14 +0530

    Chapter3
    Chapter3

---
+++ multiplication/src/test/java/booktutorial/multiplication/MultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package booktutorial.multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
